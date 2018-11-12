fe186a55a7a5c6cd707522b590e942fe3774b417
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2018-11-12 11:45:09 -0700

    fixed maze files reset main
    removed extra white space in maze files and reset main as the change was unneeded

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         Maze m5 = new Maze("5x5maze.txt");
-
-        Maze m7 = new Maze("7x7maze.txt");
-
-        Maze m8 = new Maze("8x8maze.txt");
-
-        Maze m9 = new Maze("9x9maze.txt");
-
-        Maze m10 = new Maze("10x10maze.txt");
-
-        Maze m12 = new Maze("12x12maze.txt");
-
-        Maze m14 = new Maze("14x14maze.txt");
     }
 }

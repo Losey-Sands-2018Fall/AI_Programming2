5026ef4b3ab173e981d76a00f5ed21089901caa9
Joshua Stephenson-Losey <JDSL1996@hotmail.com>
2018-11-12 11:08:58 -0700

    changed report
    made report a .txt so its easier to look at while coding. added some lines to main to make each of the current mazes, will likely change later

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Maze m = new Maze("5x5maze.txt");
+        MazeFive m = new Maze("5x5maze.txt");
+
+        MazeSeven m = new Maze("7x7maze.txt");
+
+        MazeEight m = new Maze("8x8maze.txt");
+
+        MazeNine m = new Maze("9x9maze.txt");
+
+        MazeTen m = new Maze("10x10maze.txt");
+
+        MazeTwelve m = new Maze("12x12maze.txt");
+
+        Mazefourteen m = new Maze("14x14maze.txt");
     }
 }

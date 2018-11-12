157f5f34ce95e9fb3d387f6d1664dd1e1df86497
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2018-11-12 11:30:33 -0700

    fixed main
    fixed a naming mistake in main, added my output folder to gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        MazeFive m = new Maze("5x5maze.txt");
+        Maze m5 = new Maze("5x5maze.txt");
 
-        MazeSeven m = new Maze("7x7maze.txt");
+        Maze m7 = new Maze("7x7maze.txt");
 
-        MazeEight m = new Maze("8x8maze.txt");
+        Maze m8 = new Maze("8x8maze.txt");
 
-        MazeNine m = new Maze("9x9maze.txt");
+        Maze m9 = new Maze("9x9maze.txt");
 
-        MazeTen m = new Maze("10x10maze.txt");
+        Maze m10 = new Maze("10x10maze.txt");
 
-        MazeTwelve m = new Maze("12x12maze.txt");
+        Maze m12 = new Maze("12x12maze.txt");
 
-        Mazefourteen m = new Maze("14x14maze.txt");
+        Maze m14 = new Maze("14x14maze.txt");
     }
 }

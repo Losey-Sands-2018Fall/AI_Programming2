d5dca40918759346d1875dbfc1171cbfdd982b83
Keefer Sands <keefer212@gmail.com>
2018-11-15 23:27:19 -0700

    Added maze filename addition in scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Maze m5 = new Maze("12x12maze.txt");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter in the maze filename:");
+        Maze m5 = new Maze(in.nextLine());
         System.out.println("\n\n");
         m5.solveMaze(new DumbSolve());
     }
